1c6d7df46560fbdd4e3a1795a7dd73cc2d5cc543
zhang1120zhang <18037708532@163.com>
2019-05-10 18:42:18 +0800

    eureka

---
+++ configclient/src/main/java/com/unisinsight/configclient/ConfigclientApplication.java
@@ -0,0 +1,15 @@
+package com.unisinsight.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConfigclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigclientApplication.class, args);
+    }
+
+}
